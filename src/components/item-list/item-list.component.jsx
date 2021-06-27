38dd4fdf735efc1f46e2db369dab7643f90ca7d4
Deepak Girdher <deepakgirdher@gmail.com>
2021-06-27 13:50:02 +0530

    Order type bar design ready. Menu items card design ready

--- src/components/item-list/item-list.component.jsx
+++ src/components/item-list/item-list.component.jsx
@@ -1,46 +1,56 @@
 import React from 'react';
+import OrderTypeBar from '../order-type-bar/order-type-bar.component';
 import './item-list.styles.scss'
 
 class ItemList extends React.Component {
   render() {
     return (
       <div className="item-list-container">
-        <div className="order-type-section">
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/takeaway.png" alt="" /></div>
-            <div className="order-type-title">Take Away</div>
+        <OrderTypeBar />
+        <div className="menu-items-container">
+          <div key="1" className="menu-item-card">
+            <div className="item-veg">&nbsp;</div>
+            <div className="item-name">Paneer Tikka Masala</div>
+            <div className="item-price">Rs. 250</div>
           </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/delivery.png" alt="" /></div>
-            <div className="order-type-title">Delivery</div>
+          <div key="2" className="menu-item-card">
+            <div className="item-veg">&nbsp;</div>
+            <div className="item-name">Tandoori Paneer Malai Tikka</div>
+            <div className="item-price">Rs. 250</div>
           </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
-            <div className="order-type-title">Table-1</div>
+          <div className="menu-item-card">
+            <div className="item-non-veg">&nbsp;</div>
+            <div className="item-name">Murg Tikka Masala</div>
+            <div className="item-price">Rs. 300</div>
           </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
-            <div className="order-type-title">Table-2</div>
+          <div key="1" className="menu-item-card">
+            <div className="item-veg">&nbsp;</div>
+            <div className="item-name">Paneer Tikka Masala</div>
+            <div className="item-price">Rs. 250</div>
           </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
-            <div className="order-type-title">Table-3</div>
+          <div key="2" className="menu-item-card">
+            <div className="item-veg">&nbsp;</div>
+            <div className="item-name">Tandoori Paneer Malai Tikka</div>
+            <div className="item-price">Rs. 250</div>
           </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
-            <div className="order-type-title">Table-4</div>
+          <div className="menu-item-card">
+            <div className="item-non-veg">&nbsp;</div>
+            <div className="item-name">Murg Tikka Masala</div>
+            <div className="item-price">Rs. 300</div>
+          </div> <div key="1" className="menu-item-card">
+            <div className="item-veg">&nbsp;</div>
+            <div className="item-name">Paneer Tikka Masala</div>
+            <div className="item-price">Rs. 250</div>
           </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
-            <div className="order-type-title">Table-5</div>
+          <div key="2" className="menu-item-card">
+            <div className="item-veg">&nbsp;</div>
+            <div className="item-name">Tandoori Paneer Malai Tikka</div>
+            <div className="item-price">Rs. 250</div>
           </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
-            <div className="order-type-title">Table-6</div>
-          </div>
-          <div className="order-type-item">
-            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
-            <div className="order-type-title">Table-7</div>
+          <div className="menu-item-card">
+            <div className="item-non-veg">&nbsp;</div>
+            <div className="item-name">Murg Tikka Masala</div>
+            <div className="item-price">Rs. 300</div>
           </div>
         </div>
       </div>
---
+++ src/components/order-type-bar/order-type-bar.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import "./order-type-bar.styles.scss"
+
+class OrderTypeBar extends React.Component {
+  render() {
+    return (
+      <div className="order-type-bar-container">
+        <div className="order-type-section">
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/takeaway.png" alt="" /></div>
+            <div className="order-type-title">Take Away</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/delivery.png" alt="" /></div>
+            <div className="order-type-title">Delivery</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
+            <div className="order-type-title">Table-1</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
+            <div className="order-type-title">Table-2</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
+            <div className="order-type-title">Table-3</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
+            <div className="order-type-title">Table-4</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
+            <div className="order-type-title">Table-5</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
+            <div className="order-type-title">Table-6</div>
+          </div>
+          <div className="order-type-item">
+            <div className="order-type-image"><img src="/assets/images/icons/table.png" alt="" /></div>
+            <div className="order-type-title">Table-7</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default OrderTypeBar;
